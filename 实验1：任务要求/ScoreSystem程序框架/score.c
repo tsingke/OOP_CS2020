8b2ca952068922e5a4336d4b953d4b9de4cf2439
Qingke Zhang(张庆科) <tsingke@hotmail.com>
2020-09-22 22:17:20 +0800

    程序设计参考框架

---
+++ 实验1：任务要求/ScoreSystem程序框架/score.c
@@ -0,0 +1,163 @@
+/*************************************************
+** 源文件   : score.c
+** 功能说明 : Function Definitions
+** 创建版本 : v1.0
+/**************************************************/
+
+/*----------------头文件--------------*/
+#define  _CRT_SECURE_NO_WARNINGS
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "SCORE.h"
+
+/*----------------函数定义-------------*/
+
+//1.1手动输入学生基本数据
+void readData(SS stu[], int N)
+{
+
+	printf("请按照如下格式输入学生信息：学号,姓名,平时成绩,期末成绩\n");
+
+	for (int i = 0; i < N; i++)
+	{
+		printf("第%d个学生:", i + 1);
+		scanf("%s %s %f %f", &stu[i].number, &stu[i].name, &stu[i].dailyScore, &stu[i].finalScore);
+		printf("\n");
+	}
+
+	printf("------成绩录入完毕!--------\n");
+
+}
+
+//1.2从文件里读取学生基本数据
+SS* readDataFromFile(int *N)
+{
+
+	printf("\n\n------第一步: 从文件读取学生的成绩信息--------\n\n");
+
+	SS *stu;// 开辟新空间,存取文件中的每个学生信息
+
+	FILE *fp = NULL;
+	int count = 0;
+	int index = 0;
+
+	fp = fopen("data.txt", "r");
+
+	//1.获取学生数目
+	if (fp != NULL)
+	{
+		fscanf(fp, "%d", &count);
+		*N = count;
+	}
+	else
+	{
+		printf("failed to open the info file\n");
+		getchar();
+	}
+
+	printf("学生数目为:%d\n", count);
+
+
+	//2.给所有学生分配存储空间
+	stu = (SS*)malloc(count * sizeof(SS));
+
+
+	//3.读取每条学生的信息
+	while ((!feof(fp)))
+	{
+		
+		
+		
+		
+		
+		
+		
+
+		// 请完善代码!
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+	getchar();
+
+	fclose(fp);
+
+	return stu;
+}
+
+//2.计算N个学生各自的总评成绩
+void calcuScore(SS stu[], int N)
+{
+
+
+	printf("\n\n------第二步: 计算每个学生的总评成绩--------\n\n");
+
+	
+	
+	
+	
+	// 请完善代码!
+	
+	
+	
+	
+	
+	
+
+	getchar();
+}
+
+
+//3.根据总评成绩排名
+int cmpBigtoSmall(const void *a, const void *b)
+{
+
+	SS *aa = (SS *)(a);
+	SS *bb = (SS *)(b);
+
+
+
+	if ((*aa).generalScore < (*bb).generalScore)  return 1;
+
+	else if ((*aa).generalScore > (*bb).generalScore)  return -1;
+
+	else
+		return 0;
+
+}
+
+void sortScore(SS stu[], int N)
+{
+
+	qsort(&(stu[0]), N, sizeof(stu[0]), cmpBigtoSmall);
+
+}
+
+//4.按照一定的格式输出N个学生的信息
+void printOut(SS stu[], int N)
+{
+
+	printf("\n------第三步: 根据总成绩输出学生排名信息!------\n\n");
+
+
+
+
+
+
+     // 请完善代码!
+
+
+
+
+
+	getchar();
+
+}
---
+++ 实验1：任务要求/ScoreSystem程序框架/score.h
@@ -0,0 +1,46 @@
+
+/*************************************************
+* Head File   : SCORE.h
+* File Usage  : 学生管理系统头文件
+* Create Time : v1.0
+/**************************************************/
+
+#ifndef __SCORE_H__
+#define __SCORE_H__
+
+
+#include <stdio.h>
+
+/*----------------------------------*
+	   学生信息-结构体设计
+*-----------------------------------*/
+
+typedef struct student
+{
+	char number[10];  //学号
+	char name[10];    //姓名
+	float dailyScore;   //平时成绩
+	float finalScore;   //期末成绩
+	float generalScore; //总评成绩
+
+}SS;
+
+
+/*---------------函数声明-------------------*/
+
+//1.读取学生基本数据
+void readData(SS stu[], int N);
+SS* readDataFromFile(int *N);
+
+//2.计算N个学生各自总成绩
+void calcuScore(SS stu[], int N);
+
+
+//3.根据总评成绩排名
+void sortScore(SS stu[], int N);
+
+
+//4.按照一定的格式输出N个学生的完整信息
+void printOut(SS stu[], int N);
+
+#endif

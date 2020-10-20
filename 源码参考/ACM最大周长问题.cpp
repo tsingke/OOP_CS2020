5986be2e50607e1a4e7ad1dc7a7a8b93628b85f5
Qingke Zhang(张庆科) <tsingke@hotmail.com>
2020-10-20 23:52:24 +0800

    Update ACM最大周长问题.cpp

--- 源码参考/ACM最大周长问题.cpp
+++ 源码参考/ACM最大周长问题.cpp
@@ -1,9 +1,11 @@
+```c++
 /*************************************************
-** 功能 : ACM-木棍最大周长问题
-** 作者 : tsingke
-** 创建 : 2020-10-19 / 08:38
-** 版权 : tsingke@sdnu.edu.cn
+** 功能 :  ACM木棍最大周长面积问题
+** 作者 : Qingke Zhang/tsingke@sdnu.edu.cn
+** 版本 : 2020-10-20 / 16:59
+** 版权 : GNU General Public License(GNU GPL)
 /**************************************************/
+
 
 #include <iostream>
 #include <cstdlib>
@@ -33,7 +35,7 @@ double perim_c = 0.0;
 *-----------------------------------*/
 
 //计算是否构成三角形
-int isTriangle(const double &r1,const double &r2, const double&r3)
+int isTriangle(const double &r1, const double &r2, const double&r3)
 {
 	if ((r1 + r2 > r3) && (r1 + r3 > r2) && (r2 + r3 > r1))
 	{
@@ -66,18 +68,20 @@ double area(const double &r1, const double &r2, const double&r3)
 
 
 
-/*----------------------------------*
-  三角形构建算法:第一种,复杂度o(n^3))
-*-----------------------------------*/
+/*---------------------------------------*
+   最大周长面积搜索算法:第一种,复杂度o(n^3))
+*-----------------------------------------*/
+
+//搜索最大周长
 int searchMaxLength(const double *plen, const int num)
 {
 	bool canConstructTriangle = false;
 
 	for (int i = 0; i < num; i++)
 	{
-		for (int j = i+1; j < num; j++)
+		for (int j = i + 1; j < num; j++)
 		{
-			for (int k = j+1; k < num; k++)
+			for (int k = j + 1; k < num; k++)
 			{
 
 				if (isTriangle(plen[i], plen[j], plen[k]))
@@ -114,9 +118,9 @@ int searchMaxLength(const double *plen, const int num)
 	else
 		return 1;
 
-
 }
 
+//搜索最大面积
 int searchMaxArea(const double *plen, const int num)
 {
 	bool canConstructTriangle = 0;
@@ -131,7 +135,7 @@ int searchMaxArea(const double *plen, const int num)
 				if (isTriangle(plen[i], plen[j], plen[k]))
 				{
 					double tempArea = area(plen[i], plen[j], plen[k]);
-					
+
 
 					if (tempArea > maxArea)
 					{
@@ -166,73 +170,121 @@ int searchMaxArea(const double *plen, const int num)
 
 }
 
-/*----------------------------------*
-  三角形构建算法:第二种,复杂度o(n^log(n)))
-*-----------------------------------*/
+/*----------------------------------------------*
+    最大周长面积搜索算法:第二种,复杂度o(n*log(n)))
+*-----------------------------------------------*/
 
 int cmpfunc(const void* a, const void* b)
 {
-	if (*(double*)b - *(double*)a > 0)
-	{
-		return 1;
-	}
-	else if (*(double*)b - *(double*)a < 0)
-	{
-		return -1;
-	}
 
-	else
-		return 0;
-	
+	return ((*(double*)b) - (*(double*)a));
+
 }
 
-int searchMaxLength_log(double *plen, const int num)
+//快速寻找最大周长
+int searchMaxLength_nlogn(double *plen, const int num)
 {
 
 	//1. 按照木棍长度从大到小排序
 	qsort(plen, num, sizeof(plen[0]), cmpfunc);
 
 
+
+
 	//2. 构建三角形,输出最大周长
 	bool canConstructTriangle = 0;
 
-	for (int i = 0; i < num; i++)
+	
+	int i=1, j=2, k=3;
+
+	while (k<=num)
 	{
-		for (int j = i + 1; j < num; j++)
+		if (isTriangle(plen[i],plen[j],plen[k]))
 		{
-			for (int k = j + 1; k < num; k++)
+			double tempPerimeter = perimeter(plen[i], plen[j], plen[k]);
+
+			if (tempPerimeter > maxPerimeter)
 			{
+				maxPerimeter = tempPerimeter;
 
-				if (isTriangle(plen[i], plen[j], plen[k]))
-				{
-					double tempPerimeter = perimeter(plen[i], plen[j], plen[k]);
+				//记录当前边长
+				perim_a = plen[i];
+				perim_b = plen[j];
+				perim_c = plen[k];
 
-					if (tempPerimeter > maxPerimeter)
-					{
-						maxPerimeter = tempPerimeter;
-
-						//记录当前边长
-						perim_a = plen[i];
-						perim_b = plen[j];
-						perim_c = plen[k];
-
-					}
-
-					canConstructTriangle = 1;//存在构成三角形的木棍
-
-					break;//找到后就终止搜索,不再往下搜索
-
-				}
-
-				else
-					continue;
 			}
+
+			i++;
+			j++;
+			k++;
+
+			canConstructTriangle = 1;//存在构成三角形的木棍
+
 		}
+		else
+			continue;
+
 	}
+
 
 	if (canConstructTriangle == 0)
 	{
-		
+
+		return 0;
+
+	}
+	else
+		return 1;
+
+}
+
+//快速寻找最大面积
+int searchMaxArea_nlogn(double* plen, const int num)
+{
+
+	//1. 按照木棍长度从大到小排序
+	qsort(plen, num, sizeof(plen[0]), cmpfunc);
+
+	//2. 构建三角形,输出最大面积
+	bool canConstructTriangle = 0;
+
+
+	int i = 1, j = 2, k = 3;
+
+	while (k <= num)
+	{
+		if (isTriangle(plen[i], plen[j], plen[k]))
+		{
+			double tempArea = area(plen[i], plen[j], plen[k]);
+
+
+			if (tempArea > maxArea)
+			{
+				maxArea = tempArea;
+
+				//记录当前边长
+				area_a = plen[i];
+				area_b = plen[j];
+				area_c = plen[k];
+			}
+
+			i++;
+			j++;
+			k++;
+
+			canConstructTriangle = 1;//存在构成三角形的木棍
+
+		}
+
+		else
+			continue;
+
+	}
+
+
+	if (canConstructTriangle == 0)
+	{
+
 		return 0;
 
 	}
@@ -244,7 +296,7 @@ int searchMaxLength_log(double *plen, const int num)
 
 
 /*----------------------------------*
-	      主函数
+		  主函数
 *-----------------------------------*/
 
 int main()
@@ -265,7 +317,7 @@ int main()
 
 	double *len = new double[m];
 
-	cout << "请依次输入" << m << "根木棍的长度"<<endl;
+	cout << "请依次输入" << m << "根木棍的长度" << endl;
 
 	for (int i = 0; i < m; i++)
 	{
@@ -274,18 +326,18 @@ int main()
 
 
 	}
-	
 
-	cout <<endl<<"-------------- 输出结果 --------------" << endl;
+
+	cout << endl << "-------------- 输出结果 --------------" << endl;
 
 
 	/*----------------------------------*
-	      2. 搜索最大周长,并输出周长
+		  2. 搜索最大周长,并输出周长
 	*-----------------------------------*/
 
 
 	//int success = searchMaxLength(len, m); //复杂度高
-	int length_success = searchMaxLength_log(len, m);//复杂度低
+	int length_success = searchMaxLength_nlogn(len, m);//复杂度低
 
 	if (length_success == 1)
 	{
@@ -297,13 +349,15 @@ int main()
 		cout << 0 << endl;
 		cout << "提示: 当前木棍长度无法构成任何三角形!" << endl;
 	}
-	
+
 
 	/*----------------------------------*
 		  3. 搜索最大面积,并输出面积
 	*-----------------------------------*/
 
-	int area_success = searchMaxArea(len, m);
+	//int area_success = searchMaxArea(len, m);
+
+	int area_success = searchMaxArea_nlogn(len, m);
 
 	if (area_success == 1)
 	{
@@ -313,13 +367,13 @@ int main()
 	else
 	{
 		cout << 0 << endl;
-		cout << "提示: 当前木棍长度无法构成任何三角形!"<<endl;
+		cout << "提示: 当前木棍长度无法构成任何三角形!" << endl;
 	}
 
 
 
 	/*----------------------------------*
-	       4.释放动态内存空间
+		   4.释放动态内存空间
 	*-----------------------------------*/
 
 
@@ -330,3 +384,7 @@ int main()
 
 	return 0;
 }
+	
+	
+	
+```
